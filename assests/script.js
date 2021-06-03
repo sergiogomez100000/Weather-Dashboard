97098a9303d8522647a479d6ba4f32b4c945c957
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-03 13:49:15 -0700

    upgrade layout

--- assests/script.js
+++ assests/script.js
@@ -1,22 +1,10 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-//get api to get into server
 const api_key = "16d3b60db4cf8bb617eb4d8b62e0b4f1";
 // creates var for submit form
-const submitBtn = document.querySelector(".submit-form");
+const searchBtn = document.querySelector("#search-Btn");
 // creates variable to sets value to key from local storage
 var searchHistory = localStorage.getItem("Cities");
 // creates variable for forecast container
-const forecastContainer = document.querySelector(".forecast");
+const forecastContainer = document.querySelector("#forecast");
 // console.log(forecastContainer);
 // if search history has a string then it would be tru
 if (searchHistory) {
@@ -29,42 +17,49 @@ if (searchHistory) {
 }
 // logs what type of data search History has, object
 //console.log(typeof searchHistory)
-createCityBtns();
+createHistoryBtns();
 //function to create cityBtns
-function createCityBtns() {
-  var btncontainer = document.querySelector(".search-history");
-  btncontainer.innerHTML = "";
+function createHistoryBtns() {
+  var historycontainer = document.querySelector("#search-history");
+  historycontainer.innerHTML = "";
 
   //for each cityInput run function with cityInput parameter
   searchHistory.forEach(function (cityInput) {
     //create variable to create button
-    var cityBtn = document.createElement("button");
+    var cityBtn = document.createElement("li");
+    cityBtn.className = "list-group-item text-center"
     // hav text content of cityBtn be the cityInput
     cityBtn.textContent = cityInput;
-    //appending cityBtn to btncontainer
-    btncontainer.append(cityBtn);
+    cityBtn.addEventListener("click",function(event){
+      getWeather(event.target.textContent)
+      getFiveDay(event.target.textContent)
+    })
+    //appending cityBtn to historycontainer
+    historycontainer.append(cityBtn);
   });
 }
-//create function for when submit button clicked
-function submitForm(event) {
+//create function for when search button clicked
+function submitSearch(event) {
   //prevents page refresh
   event.preventDefault();
-  const cityInput = document.querySelector("#input-city").value;
+  //creates var for city input
+  const cityInput = document.querySelector("#city-input").value;
+  if(!cityInput){
+    alert("Please input a City name!")
+  }
   getWeather(cityInput);
   getFiveDay(cityInput);
   if (searchHistory.includes(cityInput)) {
     return;
   }
-  //creates variable for city inputted in form
-
   //logs city submitted
   searchHistory.push(cityInput);
   //saves key value pair "city",searchhistory array turns into string with JSON.stringify to local storage
   localStorage.setItem("Cities", JSON.stringify(searchHistory));
-  createCityBtns();
+  createHistoryBtns();
 }
 //creates listener for when submitBtn submitted, executes function
-submitBtn.addEventListener("submit", submitForm);
+searchBtn.addEventListener("click", submitSearch);
 
 // creates function to get city weather using cityInput as parameter
 function getWeather(cityInput) {
@@ -81,7 +76,7 @@ function getWeather(cityInput) {
       //if the weather datas code is equal to 404(error message)
       if (weather.cod === "404") {
         //gives alert if error 404 code
-        alert("city not found");
+        alert("City not found! Try again!");
         return;
       }
       //retrieves lat and lon data from weather data
@@ -95,14 +90,14 @@ function getWeather(cityInput) {
         .then(function (onecallURL) {
           console.log(onecallURL);
           console.log(onecallURL.current)
-          // creates variable for element with class current -weather
-          var currentWeather = document.querySelector(".current-weather");
+          // creates variable for element with id current -weather
+          var currentWeather = document.querySelector("#current-weather");
           currentWeather.innerHTML = "";
           var d = new Date();
           var date = d.getUTCDate();
           var month = d.getUTCMonth() + 1;
           var year = d.getUTCFullYear();
-          //creates variable for name of city
+          //creates variable for name of city and date
           var name = document.createElement("h3");
           name.textContent =
             weather.name + " (" + month + "/" + date + "/" + year + ")";
@@ -148,7 +143,7 @@ function getFiveDay(cityInput) {
 
       for (var i = 4; i < fiveDayUrl.list.length; i = i + 8) {
         var forecastCard = document.createElement("div");
-        forecastCard.className = ("card bg-primary text-white")
+        forecastCard.className = ("card bg-primary text-center text-white col-md-2")
         //forecastCard.classList.add("forecast");
 
         // create  element for var, sets innertext of var to data, append var to container
